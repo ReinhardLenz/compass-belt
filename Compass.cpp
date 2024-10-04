155bd5adc5cbe48157fd93c8bc8cde9bb496b9ba
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2024-10-04 17:51:13 +0300

    Update Compass.cpp

--- Compass.cpp
+++ Compass.cpp
@@ -1,76 +1,52 @@
-#include "CompassBelt.h"
-const char* CompassBelt::Direction_names[8] = {"N", "NE", "E", "SE", "S", "SW", "W", "NW"};
-
-CompassBelt::CompassBelt(HapticBelt* belt, unsigned long vibrationDuration, unsigned long vibrationInverval)
-{
-    belt_ = belt;
-    onDuration_ = vibrationDuration;
-    vibrationInterval_ = vibrationInverval;
-    lastOn_ = 0;
-    lastDirection_ = -1;
-}
-
-void CompassBelt::setAlwaysOn(bool alwaysOn){
-    alwaysOn_ = alwaysOn;
-}
-
-bool CompassBelt::isAlwaysOn(){
-    return alwaysOn_;
-}
-
-void CompassBelt::off()
-{
-  belt_->off();
-}
-
-void CompassBelt::update(double heading)
-{
-
-
-    if (heading < 0 || heading >= 360)
-    {
-        // Contract violation
-        return;
+#include "Compass.h"
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees) {
+    // Normalize quaternion before conversion.
+    float qlength = sqrt(sq(qr) + sq(qi) + sq(qj) + sq(qk));
+    qr /= qlength;
+    qi /= qlength;
+    qj /= qlength;
+    qk /= qlength;
+    float sqr = sq(qr);
+    float sqi = sq(qi);
+    float sqj = sq(qj);
+    float sqk = sq(qk);
+    ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqr - sqj - sqk + sqr));
+    ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqr + sqj + sqk + sqr));
+    ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqr - sqj + sqk + sqr));
+    if (degrees) {
+        ypr->yaw *= RAD_TO_DEG;
+        ypr->pitch *= RAD_TO_DEG;
+        ypr->roll *= RAD_TO_DEG;
     }
-
-    const int direction_threshold = 5;
-
-    int direction = (int)(round(heading / 45) * 45) % 360;
-    // Add a buffer zone
-    if (lastDirection_ != -1 && abs(direction - heading) > direction_threshold)
-    {
-        direction = lastDirection_;
+}
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees) {
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+float getNorthDirection(float yaw) {
+    // Returns degrees East of North.
+    if (yaw > -90 && yaw < 180) { // Quadrants I, II, IV
+        return (90 - yaw);
+    } else { // Quadrant III
+        return -(yaw + 270);
     }
-
-    if (direction != lastDirection_)
-    {
-        if (lastDirection_ != -1){
-            belt_->off(lastDirection_);
-        }
-        lastDirection_ = direction;
-        belt_->on(direction, 255);
-        lastOn_ = millis();
+}
+// Modified getHeading function to accept sensor objects as arguments
+float Compass::getHeading(Adafruit_BNO08x* bno08x, sh2_SensorValue_t* sensorValue) {
+    if (bno08x->wasReset()) {
+        Serial.print("sensor was reset ");
+        bno08x->enableReport(SH2_ROTATION_VECTOR, reportIntervalUs);  // Call the enableReport function
     }
-
-    if(shouldStartVibrating()) {
-        belt_->on(direction, 255);
-        lastOn_ = millis();
-    } else if (shouldStopVibrating())
-    {
-        belt_->off(direction);
-    } 
-//    Serial.print(" A: ");
-//    Serial.println(Direction_names[direction/45]);
-
-    //Serial.println(HapticBelt(Direction_names[direction]));
-
+    if (bno08x->getSensorEvent(sensorValue)) {
+        acc_status = sensorValue->status;
+        quaternionToEulerRV(&sensorValue->un.rotationVector, &ypr, true); // degrees
+        yaw = ypr.yaw;
+        pitch = ypr.pitch;
+        roll = ypr.roll;
+        accuracy = sensorValue->un.rotationVector.accuracy;
+    }
+    float currentHeading = getNorthDirection(ypr.yaw);
+    delay(BNO085_SAMPLERATE_DELAY_MS);
+    Serial.println(currentHeading);
+    return currentHeading;
 }
 
-bool CompassBelt::shouldStopVibrating(){
-    return !alwaysOn_ && millis() - lastOn_ >= onDuration_;
-}
-
-bool CompassBelt::shouldStartVibrating(){
-    long timeSinceOn = millis() - lastOn_;
-    return alwaysOn_ || timeSinceOn >= onDuration_ + vibrationInterval_;
-}
