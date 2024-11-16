af55ab66b887e38d46f90fed1653a50a0cb28a11
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2024-11-16 11:23:21 +0200

    Update Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -3,32 +3,43 @@
 Button::Button(int pin)
 {
   this->pin = pin;
-  pinMode(pin, INPUT_PULLUP);  
+  pinMode(pin, INPUT);  
+  wasPressed = false;  // Initialize wasPressed to false
 }
 
 ButtonState Button::read()
 {
-  // TODO: Add debouncer
   bool pressed = digitalRead(pin);
-  //Serial.println("button pressed");
   bool down = pressed && !wasPressed;
-  bool up = !pressed && wasPressed;
-  long pressDuration = 0;
+// TRUTH TABLE
+// | pressed | wasPressed  | !wasPressed | pressed && !wasPressed |
+// |---------|-------------|-------------|------------------------|
+// |    0    |      0      |      1      |           0            |
+// |    0    |      1      |      0      |           0            |
+// |    1    |      0      |      1      |           1            |
+// |    1    |      1      |      0      |           0            |
 
+  
+  bool up = !pressed && wasPressed;
+// TRUTH TABLE
+// | pressed | wasPressed  |   !pressed  |!pressed && wasPressed  |
+// |---------|-------------|-------------|------------------------|
+// |    0    |      0      |      1      |           0            |
+// |    0    |      1      |      1      |           1            |
+// |    1    |      0      |      0      |           0            |
+// |    1    |      1      |      0      |           0            |
+
+  long pressDuration = 0;
   if (down){
     lastDown = millis();  
   }
-
   if (up){
     lastUp = millis();  
     pressDuration = lastUp - lastDown;
   }
-
   wasPressed = pressed;
-
   bool isLongPress;
   bool isSinglePress;
-
   if (pressed){
     isLongPress = millis() - lastDown >= 1000;
   } else if (up){
@@ -36,26 +47,20 @@ ButtonState Button::read()
   } else {
     isLongPress = false;
   }
-
   if (up){
     isSinglePress = pressDuration < 500 && pressDuration > 20;
   } else {
     isSinglePress = false;
   }
-
   bool isDoublePress = isSinglePress && millis() - lastSinglePress < 500 && millis() - lastDoublePress > 1000;
-
   if (isDoublePress){
     lastDoublePress = millis();  
   }
-
   if (isSinglePress){
     lastSinglePress = millis();  
   }
-
   bool sendLongPress = isLongPress && !wasLong;
   wasLong = isLongPress;
-
   return ButtonState {
     pressed,
     down,
