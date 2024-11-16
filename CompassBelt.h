e28314720a2639ee31435fa988ff4a239254dd6f
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2024-11-16 11:21:51 +0200

    Update CompassBelt.h

--- CompassBelt.h
+++ CompassBelt.h
@@ -3,25 +3,27 @@
 
 #include "HapticBelt.h"
 #include <Arduino.h>
-
+extern int belt_pins[]; 
 class CompassBelt
 {
     public:
         CompassBelt(HapticBelt* belt, unsigned long vibrationDuration, unsigned long vibrationInverval);
         void update(double heading);
         void off();
+        void lampTest();
         void setAlwaysOn(bool alwaysOn);
         bool isAlwaysOn();
         
     private:
         HapticBelt* belt_;
-        static const char* Direction_names[8];
+//        static const char* Direction_names[20];
         unsigned long onDuration_;
         unsigned long vibrationInterval_;
         long lastOn_;
         int lastDirection_;
-        bool alwaysOn_ = false;
+        bool alwaysOn_ = true;
         bool shouldStopVibrating();
         bool shouldStartVibrating();
+        
 };
 #endif
