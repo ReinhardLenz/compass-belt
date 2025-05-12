58205eead5969841fb8bd0728f202a4ae9c60fcf
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2025-05-12 17:10:10 +0300

    Update potentiometer.cpp

--- potentiometer.cpp
+++ potentiometer.cpp
@@ -1,13 +1,10 @@
 #include "potentiometer.h"
 #include <Arduino.h>
+
 void sensorValueToDegree(int &degree_shift) {
-    int sensorValue = analogRead(A2);  // Read the analog value from pin A2
-    degree_shift = int(sensorValue / 2.86666);  // Update degree_shift
-//    Serial.print(sensorValue);
-//    Serial.print("   ");
-//    Serial.println(degree_shift);
-    // Ensure the value does not exceed 360
-    if (degree_shift > 360) {
-        degree_shift = 360;
-    }
+  int sensorValue = analogRead(A2);
+  degree_shift = int(sensorValue / 1.86329);
+  if (degree_shift > 360) {
+    degree_shift = 360;
+  }
 }
