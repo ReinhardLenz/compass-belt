8727b55af33d6b7d46af489d3140bea7c13a5faa
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2025-05-12 17:08:50 +0300

    Update HapticBelt.h

--- HapticBelt.h
+++ HapticBelt.h
@@ -2,65 +2,65 @@
 #define Haptic_Belt_H
 
 #include <Arduino.h>
- //A haptic belt with the following directions:
-//DIRECTION    PIN
-//   1          34   
-//   2          35
-//   3          36  
-//   4          37
-//   5          38  
-//   6          39
-//   7          40  
-//   8          41
-//   9          42   
-//   10         43
-//   11         44   
-//   12         45
-//   13         46   
-//   14         47
-//   15         48   
-//   16         49
-//   17         50   
-//   18         51
-//   19         52
-//   20         53
- 
-//                           1                           
-//                   20               2                  
-//           19                              3           
-//                                                      
-//                                                      
-//     18                                           4    
-//                                                      
-//                                                      
-//                                                      
-//  17                                                  5 
-//                                                      
-//                                                      
-//16                                                     6
-//                                                      
-//                                                      
-//  15                                                 7 
-//                                                      
-//                                                      
-//                                                      
-//      14                                          8    
-//                                                      
-//                                                      
-//           13                                 9        
-//                  12                 10                 
-//                           11                          
-
 
 class HapticBelt
 {
-    private:
-        int haptic_pins_[20];
-        bool isDirectionValid(int direction);
-    public:
-        HapticBelt(int haptics[20]);
-        void on(int direction, int power);
-        void off(int direction);
-        void off();
+  private:
+    int haptic_pins_[20];
+    bool isDirectionValid(int direction);
+  public:
+    HapticBelt();
+    void on(int direction, int power);
+    void off(int direction);
+    void off();
 };
+
 #endif
+//A haptic belt with the following directions:
+//DIRECTION    PIN
+//1:24
+//2:25
+//3:26
+//4:27
+//5:28
+//6:29
+//7:30
+//8:31
+//9:32
+//10:33
+//11:34
+//12:35
+//13:36
+//14:37
+//15:38
+//16:39
+//17:40
+//18:41
+//19:42
+//20:43
+// in brackets the angle
+//                           1(0)
+//                   20(342)           2(18)
+//           19(324)                          3(36)
+//
+//
+//     18(306)                                       4(54)
+//
+//
+//
+//  17(288)                                              5(72)
+//
+//
+//16(270)                        .                        6(90)
+//
+//
+//  15(252)                                             7(108)
+//
+//
+//
+//      14(234)                                      8(126)
+//
+//
+//           13(216)                             9(144)
+//                  12(198)             10(162)
+//                           11(180)
