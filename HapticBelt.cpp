e15225c191bec39305e93722e39ff71fd2caec50
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2025-06-27 07:52:55 +0300

    Update HapticBelt.cpp

--- HapticBelt.cpp
+++ HapticBelt.cpp
@@ -4,7 +4,7 @@
 //CONSTRUCTOR
 // Initializes the haptic pins to the specified pin numbers and sets them as OUTPUT pins
 HapticBelt::HapticBelt() {
-int pins[20] = {26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45};
+int pins[20] = {24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43};
 
   for (int i = 0; i < 20; i++) {
     haptic_pins_[i] = pins[i];
